6af6cf7f68c32a905e54677385be6d523c7a8959
Kevin Kouomeu <kevinkouomeu@icloud.com>
2023-11-12 20:05:04 +0100

    :art: Fix log messages

--- backend/src/helpers/Logger.js
+++ backend/src/helpers/Logger.js
@@ -1,10 +1,10 @@
 import winston from 'winston';
 
 export const Logger = winston.createLogger({
-    level: "info",
+    level: 'info',
     transports: [new winston.transports.Console()],
     format: winston.format.combine(
         winston.format.timestamp({format :  'DD-MM-YYYY [at] HH:mm:ss'}),
-        winston.format.prettyPrint({colorize: true})
-    ),
-})
+        winston.format.json({ colorize: true })
+    )
+});
