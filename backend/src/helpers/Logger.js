b94879ba0362b3fe9cdb2cb23e20ea623ff2a291
Kevin Kouomeu <kevinkouomeu@icloud.com>
2023-11-17 13:35:39 +0100

    :sparkles: Add dockerfiles for envs

--- backend/src/helpers/Logger.js
+++ backend/src/helpers/Logger.js
@@ -4,7 +4,7 @@ export const Logger = winston.createLogger({
     level: 'info',
     transports: [new winston.transports.Console()],
     format: winston.format.combine(
-        winston.format.timestamp({format :  'DD-MM-YYYY [at] HH:mm:ss'}),
+        winston.format.timestamp({format : 'DD-MM-YYYY [at] HH:mm:ss'}),
         winston.format.json()
     )
 });
