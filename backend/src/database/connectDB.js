11201f988fbd718eebc422839aba44e599b5d511
Kevin Kouomeu <kevinkouomeu@icloud.com>
2023-11-28 19:14:02 +0100

    :bug: Fix database connection issue

--- backend/src/database/connectDB.js
+++ backend/src/database/connectDB.js
@@ -1,12 +1,16 @@
 import * as mongoose from 'mongoose';
-import 'dotenv/config'
+import 'dotenv/config';
+
+const { DB_URI, DB_AUTHSOURCE, DB_USERNAME, DB_PASSWORD, PORT, NODE_ENV } = process.env;
+let URI = NODE_ENV === 'production' ? 'mongodb://host.docker.internal:27017/likely' : DB_URI;
 
 export const connectDB = async () => {
+    console.log(URI);
     try {
-        await mongoose.connect(process.env.DB_URI, {
-            authSource: process.env.DB_AUTHSOURCE,
-            user: process.env.DB_USERNAME,
-            pass: process.env.DB_PASSWORD
+        await mongoose.connect(URI, {
+            authSource: DB_AUTHSOURCE,
+            user: DB_USERNAME,
+            pass: DB_PASSWORD
         });
     } catch (error) {
         console.error(error);
